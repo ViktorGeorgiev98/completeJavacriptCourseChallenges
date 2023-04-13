9a6c5d1e18cc51e2759cc3e443f681c7c717a097
ViktorGeorgiev98 <122877168+ViktorGeorgiev98@users.noreply.github.com>
2023-04-13 16:31:09 +0300

    excersise for values and variables

---
+++ valuesAndVariables/excersise1.js
@@ -0,0 +1,7 @@
+let country = 'Bulgaria';
+let continent = 'Europe';
+let population = '7 million';
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(`${country} is a country in ${continent} with a population of ${population} people`);
